173921c90e8d9d42e3ed2ffbdb81d26aeab7cd63
Sunguk Lee <d3m3vilurr@gmail.com>
2019-08-21 01:55:21 +0900

    Initial support swaywm windows

--- window/__init__.py
+++ window/__init__.py
@@ -3,6 +3,8 @@ import sys
 
 if sys.platform == 'win32':
     from window.win32 import Win32Window as DefaultWindow
+elif os.environ.get('SWAYSOCK'):
+    from window.sway import SwayWindow as DefaultWindow
 #elif os.environ.get('WAYLAND_DISPLAY'):
 #    pass
 else:
---
+++ window/sway.py
@@ -0,0 +1,34 @@
+import i3ipc
+from window.base import BaseWindow
+
+class SwayWindow(BaseWindow):
+    def __init__(self):
+        self.i3 = i3ipc.Connection()
+        self._curr_win_rect = (0, 0, 0, 0)
+
+    def get_window_size(self):
+        if not self.is_active():
+            return (0, 0)
+        return self._curr_win_rect[-2:]
+
+    def get_window_offset(self):
+        if not self.is_active():
+            return (0, 0)
+        offset = self._curr_win_rect[:2]
+        _, height = self.get_window_size()
+        return (offset[0], offset[1] - int(height / 20))
+
+    def get_radius(self):
+        if not self.is_active():
+            return 0
+        _, height = self.get_window_size()
+        return int(height / 5)
+
+    def is_active(self):
+        try:
+            self._curr_win = self.i3.get_tree().find_focused()
+            rect = self._curr_win.rect
+            self._curr_win_rect = (rect.x / 2, rect.y / 2, rect.width / 2, rect.height / 2)
+            return self._curr_win.name == 'Path of Exile'
+        except:
+            return False
